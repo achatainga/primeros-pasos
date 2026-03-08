0c81aea45f85237fa377573488ac0bd1bc01ca1a
Alejandro Chataing <a.chataing.a@gmail.com>
2026-03-08 02:15:04 -0400

    Fix: build error useEffect no usado

--- src/pages/Estudiante.tsx
+++ src/pages/Estudiante.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 import { toast } from 'react-toastify';
